f5aa9e1530e3894475d9c9e3eb2b3e4fa22655a6
nicola.rizzo <nicola.rizzo@eb94fdc7-1d30-0410-9ff4-57b69cae4e32>
2008-11-02 19:51:46 +0000

    Code clean

--- dojox/widget/_codeTextArea/plugins/MatchingBrackets.js
+++ dojox/widget/_codeTextArea/plugins/MatchingBrackets.js
@@ -1,68 +1,75 @@
 dojo.provide("dojox.widget._codeTextArea.plugins.MatchingBrackets");
-
-dojox.widget._codeTextArea.plugins.MatchingBrackets.startup = function(args){
-	var source = args.source;
-	var brackets = [];
-	var currentBrackets = [];
-	var getX = source.getTokenX;
-
-	var isBracket = function(token){
+dojox.widget._codeTextArea.plugins.MatchingBrackets = {
+	source: null,
+	brackets: [],
+	currentBrackets: [],
+	getX: null,
+    startup: function(args){
+		var self = this;
+		this.source = source = args.source;
+		this.getX = this.source.getTokenX;
+		dojo.subscribe(source.id + "::writeToken", dojo.hitch(this, self.pushBracket));
+		dojo.subscribe(source.id + "::CaretMove", dojo.hitch(this, self.setBracketColors));
+		dojo.subscribe(source.id + "::removeCharAtCaret", dojo.hitch(this, self.setBracketColors));
+		dojo.subscribe(source.id + "::fragmentParsed", dojo.hitch(this, self.makeBracketsList));
+	    dojo.subscribe(source.id + "::viewportParsed", dojo.hitch(this, self.makeBracketsList));
+		dojo.subscribe(source.id + "::KeyPressed", dojo.hitch(this, self.gotoMatchingBracket));
+	},
+	isBracket: function(token){
 		return token.getAttribute("tokenType").indexOf("bracket") != -1;
-	};
-	var getBracketType = function(token){
+	},
+	getBracketType: function(token){
 		var tokenType = token.getAttribute("tokenType");
 		return tokenType.substring(tokenType.indexOf("-") + 1);
-	};
-
-	var getBracketStatus = function(token){
+	},
+	getBracketStatus: function(token){
 		var tokenType = token.getAttribute("tokenType");
 		return tokenType.substring(0, tokenType.indexOf("-"));
-	};
-
-	var removeColors = function(){
+	},
+	removeColors: function(){
+		var currentBrackets = this.currentBrackets;
 		for(var i = 0; i < currentBrackets.length; i++){
 			dojo.removeClass(dojo.byId(currentBrackets[i]), "matchingBracket");
 		}
 		currentBrackets.length = 0;
-	};
-
-	var makeBracketsList = function(){
+	},
+	makeBracketsList: function(){
 		var bracketsToAdd = dojo.query("[tokenType$=bracket]", source.domNode);
 		if(!bracketsToAdd.length){
 			return;
 		}
-		brackets.length = 0;
-		brackets = bracketsToAdd;
-		setBracketColors();
-	};
-
-	var colorize = function(token){
-		var bracketType = getBracketType(token);
-		var status = getBracketStatus(token);
-		var matchingStatus = status == "open" ? "closed" : "open";
-		var startIndex = -1;
-		var matchingCounter = 0;
-		
-		var increment = 1;
-		var i = 0;
-		var parsedBrackets = 0;
+		this.brackets.length = 0;
+		this.brackets = bracketsToAdd;
+		this.setBracketColors();
+	},
+	colorize: function(token){
+		var bracketType = this.getBracketType(token),
+			status = this.getBracketStatus(token),
+			matchingStatus = status == "open" ? "closed" : "open",
+			startIndex = -1,
+			matchingCounter = 0,
+			increment = 1,
+			i = 0,
+			parsedBrackets = 0,
+			brackets = this.brackets
+		;
 		if(status == "closed"){
 			increment = -1;
 			i = brackets.length - 1;
 		}
 		while(parsedBrackets < brackets.length){
 			if(startIndex != -1){
-				if(getBracketStatus(brackets[i]) == matchingStatus){
-					if(getBracketType(brackets[i]) == bracketType){
+				if(this.getBracketStatus(brackets[i]) == matchingStatus){
+					if(this.getBracketType(brackets[i]) == bracketType){
 						matchingCounter--;
 						if(!matchingCounter){
-							currentBrackets.push(brackets[startIndex]);
-							currentBrackets.push(brackets[i]);
+							this.currentBrackets.push(brackets[startIndex]);
+							this.currentBrackets.push(brackets[i]);
 							break;
 						}
 					}
 				}else{ //same status
-					if(getBracketType(brackets[i]) == bracketType){
+					if(this.getBracketType(brackets[i]) == bracketType){
 						matchingCounter++;
 					}						
 				}
@@ -75,52 +82,57 @@ dojox.widget._codeTextArea.plugins.MatchingBrackets.startup = function(args){
 			parsedBrackets++;
 			i += increment;
 		}
-		if(currentBrackets.length == 2){
+		if(this.currentBrackets.length == 2){
 			for(var i = 0; i < 2; i++){
-				dojo.addClass(currentBrackets[i], "matchingBracket");
+				dojo.addClass(this.currentBrackets[i], "matchingBracket");
 			}
 		}else{
 			// only one bracket found
-			currentBrackets.length = 0;
+			this.currentBrackets.length = 0;
 		}		
-	};
-
-	var setBracketColors = function(){
-		removeColors();
-		var token = source.currentToken;
-		if(isBracket(token)){
-			colorize(token);
+	},
+	setBracketColors: function(){
+		this.removeColors();
+		var token = this.source.currentToken;
+		if(this.isBracket(token)){
+			this.colorize(token);
 		}
-	};
-	var deleteRemovedBrackets = function(){
+	},
+	deleteRemovedBrackets: function(){
+		var brackets = this.brackets;
 		for(var i = brackets.length - 1; i >= 0; i--){
 			if(!brackets[i].parentNode){
 				brackets.splice(i, 1);
 			}
 		}
-	};
-	var insertBracket = function(bracket){
-		var len = brackets.length;
-		var i = 0;
-		while(i < len && source.compareTokenPosition({token:brackets[i], index:0}, {token:bracket, index:0}) == -1){
+	},
+	insertBracket: function(bracket){
+		var brackets = this.brackets,
+			len = brackets.length,
+			i = 0
+		;
+		while(i < len && source.compareTokenPosition({ token:brackets[i], index:0 }, { token:bracket, index:0 }) == -1){
 			i++;
 		}
 		brackets.splice(i, 0, bracket);
-	};
-	var pushBracket = function(){
-		var token = source.currentToken;
-		if(isBracket(token)){
-			deleteRemovedBrackets();
-			insertBracket(token);
+	},
+	pushBracket:function(){
+		var token = this.source.currentToken;
+		if(this.isBracket(token)){
+			this.deleteRemovedBrackets();
+			this.insertBracket(token);
 		}
-	};
-	var gotoMatchingBracket = function(data){
-		var evt = data.evt;
+	},
+	gotoMatchingBracket: function(data){
+		var evt = data.evt,
+			currentBrackets = this.currentBrackets;
+		;
         if(evt.ctrlKey && evt.charCode == 98){ // ctrl + b
-			var token = source.currentToken;
-			if(isBracket(token)){
-				var targetToken = null;
-				var matchingToken = null;
+			var token = this.source.currentToken;
+			if(this.isBracket(token)){
+				var targetToken = null,
+					matchingToken = null
+				;
 				for(var i = 0; i < currentBrackets.length; i++){
 					if(currentBrackets[i] === token){
 						matchingToken = currentBrackets[i];																	
@@ -129,16 +141,9 @@ dojox.widget._codeTextArea.plugins.MatchingBrackets.startup = function(args){
 					}
 				}
 				if(targetToken && matchingToken){
-					source.setCaretPosition(getX(targetToken), source.indexOf(targetToken.parentNode));
+					this.source.setCaretPosition(this.getX(targetToken), this.source.indexOf(targetToken.parentNode));
 				}
 			}
 		}		
-	};
-	
-	dojo.subscribe(source.id + "::writeToken", pushBracket);
-	dojo.subscribe(source.id + "::CaretMove", setBracketColors);
-	dojo.subscribe(source.id + "::removeCharAtCaret", setBracketColors);
-	dojo.subscribe(source.id + "::fragmentParsed", makeBracketsList);
-    dojo.subscribe(source.id + "::viewportParsed", makeBracketsList);
-	dojo.subscribe(source.id + "::KeyPressed", gotoMatchingBracket);
+	}
 };
